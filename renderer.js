01447d03f807c08b0b3b1168969e8ce07549509f
İbrahim Sanduvaç <ihsanduvac@gmail.com>
2020-05-25 22:44:53 +0300

    first

---
+++ renderer.js
@@ -0,0 +1,32 @@
+const {BrowserWindow} = require('electron').remote;
+//var window = BrowserWindow.getFocusedWindow();
+
+var $ = require('jquery');
+
+$('#closeButton').on('click', function(){
+    var window = BrowserWindow.getFocusedWindow();
+    window.close();
+});
+
+$('#minimizeButton').on('click', function(){
+    var window = BrowserWindow.getFocusedWindow();
+    window.minimize();
+});
+
+$('#maximizeButton').on('click', function(){
+    var window = BrowserWindow.getFocusedWindow();
+    if(window.isMaximized()){
+        window.unmaximize();
+    }else{
+        window.maximize();
+    }
+});
+
+$('#devToolsButton').on('click', function(){
+    var window = BrowserWindow.getFocusedWindow();
+    window.webContents.openDevTools();
+});
+
+$('#sidebarButton').on('click', function(){
+    $('.sidebar-row').toggleClass('open');
+});
